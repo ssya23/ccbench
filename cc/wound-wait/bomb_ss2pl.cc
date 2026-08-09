ce17eb8c60b7328db3701c4ccabcfd6875f0454a
ssya23 <190173848+ssya23@users.noreply.github.com>
2026-08-09 15:14:03 +0900

    ss2PLをコピーしたwound-waitフォルダを追加

---
+++ cc/wound-wait/bomb_ss2pl.cc
@@ -0,0 +1,59 @@
+#define GLOBAL_VALUE_DEFINE
+
+#include "include/common.hh"
+#include "include/result.hh"
+#include "include/transaction.hh"
+#include "include/util.hh"
+
+// For BoMB with next-key/gap lock like phantom avoidance.
+#include "../../include/bomb_pessimistic.hh"
+#include "../../include/cpu.hh"
+#include "../../include/debug.hh"
+#include "../../include/fileio.hh"
+#include "../../include/masstree_wrapper.hh"
+#include "../../include/result.hh"
+#include "../../include/tsc.hh"
+#include "../../include/util.hh"
+
+#include "../../common/runner.hh"
+
+using namespace std;
+
+int main(int argc, char* argv[]) try {
+  gflags::SetUsageMessage("BOMB SS2PL benchmark.");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  chkArg();
+  BombWorkload<Tuple, void>::displayWorkloadParameter();
+  BombWorkload<Tuple, void>::makeDB(nullptr);
+
+  initResult(TotalThreadNum);
+
+  ccbench::RunnerOptions opts;
+  opts.display_per_tx = true;
+  opts.enable_bomb_dispatcher = FLAGS_bomb_mixed_mode;
+
+  ccbench::run<TxExecutor, TransactionStatus, BombWorkload<Tuple, void>>(
+      TotalThreadNum, opts,
+      [](size_t thid, const bool& quit, Backoff& /*unused*/) {
+        return TxExecutor(thid, &CCBenchResults[thid], quit);
+      },
+      [](TxExecutor& trans, size_t thid) {
+#if WAL
+        std::string logpath;
+        genLogFile(logpath, thid);
+        trans.logfile_.open(logpath, O_CREAT | O_TRUNC | O_WRONLY, 0644);
+        trans.logfile_.ftruncate(1000000000);
+#else
+        (void) trans;
+#endif
+#ifdef Linux
+        setThreadAffinity(thid);
+#endif
+#if MASSTREE_USE
+        MasstreeWrapper<Tuple>::thread_init(int(thid));
+#endif
+      },
+      ccbench::NoOpHook{}, &BombWorkload<Tuple, void>::request_dispatcher);
+
+  return 0;
+} catch (const bad_alloc&) { ERR; }
---
+++ cc/wound-wait/tpcc_ss2pl.cc
@@ -0,0 +1,47 @@
+#define GLOBAL_VALUE_DEFINE
+
+#include "include/common.hh"
+#include "include/result.hh"
+#include "include/transaction.hh"
+#include "include/util.hh"
+
+#include "../../include/cpu.hh"
+#include "../../include/debug.hh"
+#include "../../include/masstree_wrapper.hh"
+#include "../../include/result.hh"
+#include "../../include/tpcc.hh"
+#include "../../include/tsc.hh"
+#include "../../include/util.hh"
+
+#include "../../common/runner.hh"
+
+using namespace std;
+
+int main(int argc, char* argv[]) try {
+  gflags::SetUsageMessage("TPC-C SS2PL benchmark.");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  chkArg();
+  TPCCWorkload<Tuple, void>::displayWorkloadParameter();
+  TPCCWorkload<Tuple, void>::makeDB(nullptr);
+
+  initResult(TotalThreadNum);
+
+  ccbench::RunnerOptions opts;
+  opts.display_per_tx = true;
+
+  ccbench::run<TxExecutor, TransactionStatus, TPCCWorkload<Tuple, void>>(
+      TotalThreadNum, opts,
+      [](size_t thid, const bool& quit, Backoff& /*unused*/) {
+        return TxExecutor(thid, &CCBenchResults[thid], quit);
+      },
+      [](TxExecutor& /*trans*/, size_t thid) {
+#ifdef Linux
+        setThreadAffinity(thid);
+#endif
+#if MASSTREE_USE
+        MasstreeWrapper<Tuple>::thread_init(int(thid));
+#endif
+      });
+
+  return 0;
+} catch (const bad_alloc&) { ERR; }
